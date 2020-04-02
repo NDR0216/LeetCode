a2d58bd5110764296ef8d6a1023d1e80528c933b
Ander Liu <s9633051@gmail.com>
2020-04-02 22:32:02 +0800

    Create FindLuckyIntegerInAnArray.py

---
+++ FindLuckyIntegerInAnArray/FindLuckyIntegerInAnArray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        frequency = {};
+        for i in arr:
+            value = frequency.get(i, 0)
+            
+            if value != 0:
+                frequency[i] = value+1
+            else:
+                frequency[i] = 1
+                
+        ans = -1
+        for key, value in frequency.items():
+            if key == value and key > ans:
+                ans = key
+                    
+        return ans
+            
+            
+# list method
