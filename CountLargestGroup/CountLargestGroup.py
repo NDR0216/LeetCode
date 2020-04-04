c836d71126c616e3c0094ba4099e522d008888a2
Ander Liu <s9633051@gmail.com>
2020-04-05 00:17:37 +0800

    Create CountLargestGroup.py

---
+++ CountLargestGroup/CountLargestGroup.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        size = {}
+        
+        for i in range(1, n+1):
+            sum = 0
+            while(i != 0):
+                sum += i%10
+                i = int(i/10)
+            
+            value = size.get(sum, 0)
+            if value == 0:
+                size[sum] = 1
+            else:
+                size[sum] = value+1
+        
+        max = 0
+        max_cnt = 0
+        
+        for value in size.values():
+            if value > max:
+                max = value
+                max_cnt = 1
+            elif value == max:
+                max_cnt += 1
+                
+        return max_cnt
