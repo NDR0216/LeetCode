bdbe1f4e9e681bba54213ec8296da4bf72ccf8b4
Ander Liu <s9633051@gmail.com>
2020-04-08 02:12:41 +0800

    condense code

--- DesignUndergroundSystem/DesignUndergroundSystem.py
+++ DesignUndergroundSystem/DesignUndergroundSystem.py
@@ -13,23 +13,10 @@ class UndergroundSystem:
         time = t - self.record[id][1]
         
         value = self.AverageTime.get((startStation, stationName), (0, 0))
-        
-        if value == (0, 0):
-            self.AverageTime[(startStation, stationName)] = (time, 1)
-        else:
-            self.AverageTime[(startStation, stationName)] = (value[0]+time, value[1]+1)
+        self.AverageTime[(startStation, stationName)] = (value[0]+time, value[1]+1)
         return
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         sum, n = self.AverageTime[(startStation, endStation)]
         
         return float(sum/n)
-        
-        
-
-
-# Your UndergroundSystem object will be instantiated and called as such:
-# obj = UndergroundSystem()
-# obj.checkIn(id,stationName,t)
-# obj.checkOut(id,stationName,t)
-# param_3 = obj.getAverageTime(startStation,endStation)
