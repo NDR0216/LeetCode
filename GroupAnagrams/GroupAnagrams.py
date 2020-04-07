a10cb9a5b530b5c7f6be715131b837ffbd8f7e83
Ander Liu <s9633051@gmail.com>
2020-04-08 02:10:18 +0800

    condense code

--- GroupAnagrams/GroupAnagrams.py
+++ GroupAnagrams/GroupAnagrams.py
@@ -7,10 +7,7 @@ class Solution:
             chars = {}
             for j in i:
                 value = chars.get(j, 0)
-                if value == 0:
-                    chars[j] = 1
-                else:
-                    chars[i] = value+1
+                chars[j] = value+1
             
             if chars in record:
                 idx = record.index(chars)
