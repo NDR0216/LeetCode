3f9dd023c4d3f5441e32f01c83aee6d0672f84e1
Ander Liu <anderliu0216@gmail.com>
2024-04-25 16:54:35 -0500

    Update TaskSchedulerII.cpp

--- code/TaskSchedulerII/TaskSchedulerII.cpp
+++ code/TaskSchedulerII/TaskSchedulerII.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        long long days = 0; 
+        
+        map<int, long> dict;
+        for (int i = 0; i < tasks.size(); i++) {
+            if (dict.count(tasks[i]) > 0) {
+                if (days < dict[tasks[i]]) {
+                    days = dict[tasks[i]];
+                }
+            }
+            days++;
+            
+            dict[tasks[i]] = long(days + space);
+        }
+        
+        return days;
+    }
+};
