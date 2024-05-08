876dcd56117963506ca2360dd26900d7bd70939c
Ander Liu <anderliu0216@gmail.com>
2024-05-07 22:19:28 -0500

    Update LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.cpp

--- code/LongestStrictlyIncreasingOrStrictlyDecreasingSubarray/LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.cpp
+++ code/LongestStrictlyIncreasingOrStrictlyDecreasingSubarray/LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
-        int max = 1;
+        int max_length = 1;
+        int inc = 1;
+        int dec = 1;
 
         int length = 1;
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] > nums[i - 1]) {
-                length++;
+                inc++;
+                dec = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                inc = 1;
+                dec++;
             } else {
-                if (length > max) {
-                    max = length;
-                }
-                length = 1;
+                inc = 1;
+                dec = 1;
             }
-        }
-        if (length > max) {
-            max = length;
+
+            max_length = max(max_length, max(inc, dec));
         }
 
-        length = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < nums[i - 1]) {
-                length++;
-            } else {
-                if (length > max) {
-                    max = length;
-                }
-                length = 1;
-            }
-        }
-        if (length > max) {
-            max = length;
-        }
-
-        return max;
+        return max_length;
     }
 };
