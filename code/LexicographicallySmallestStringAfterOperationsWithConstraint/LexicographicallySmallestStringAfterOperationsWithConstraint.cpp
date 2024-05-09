f2ead39eaad5f519f3f2fb9d5b4a5dc625ca566a
Ander Liu <anderliu0216@gmail.com>
2024-05-09 00:08:14 -0500

    Update LexicographicallySmallestStringAfterOperationsWithConstraint.cpp

--- code/LexicographicallySmallestStringAfterOperationsWithConstraint/LexicographicallySmallestStringAfterOperationsWithConstraint.cpp
+++ code/LexicographicallySmallestStringAfterOperationsWithConstraint/LexicographicallySmallestStringAfterOperationsWithConstraint.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     string getSmallestString(string s, int k) {
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] - k < 'a' || s[i] + k > 'z') {
-                k -= min('z' + 1 - s[i], s[i] - 'a');
+            int dist_a = min(s[i] - 'a', 'z' + 1 - s[i]); // distance to 'a'
+            if (dist_a <= k) { // can be changed to 'a'
                 s[i] = 'a';
+                k -= dist_a;
             } else {
                 s[i] -= k;
                 k = 0;
