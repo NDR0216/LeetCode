5ac7dcf9e88ec30a7445958f9784d32e3b4fefaa
Ander Liu <anderliu0216@gmail.com>
2024-05-20 08:07:51 -0500

    Create NumberOfWonderfulSubstrings.cpp

---
+++ code/NumberOfWonderfulSubstrings/NumberOfWonderfulSubstrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        const int LETTER = 10;
+
+        unordered_map<bitset<LETTER>, int> hashmap;
+
+        bitset<LETTER> bitmask;
+
+        hashmap[bitmask] = 1;
+
+        long long wonderful = 0;
+        for (int i = 0; i < word.size(); i++) {
+            bitmask.flip(word[i] - 'a');
+
+            wonderful += hashmap[bitmask];
+
+            hashmap[bitmask] += 1;
+
+            for (int j = 0; j < LETTER; j++) {
+                bitmask.flip(j);
+
+                wonderful += hashmap[bitmask];
+
+                bitmask.flip(j);
+            }
+        }
+
+        return wonderful;
+    }
+};
