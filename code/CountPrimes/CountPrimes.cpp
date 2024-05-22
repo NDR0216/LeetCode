c1dcb6fb971793ae2fb20a179b3c02259e47360f
Ander Liu <anderliu0216@gmail.com>
2024-05-21 23:09:16 -0500

    Create CountPrimes.cpp

---
+++ code/CountPrimes/CountPrimes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool* SieveOfEratosthenes(int n) { // find prime[2:n-1]
+        bool* prime = new bool[n];
+        fill_n(prime, n, true);
+
+        for (int i = 2; i < sqrt(n); i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        return prime;
+    }
+
+    int countPrimes(int n) {
+        bool* prime = SieveOfEratosthenes(n);
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
