d74fcf1ef5143b31a454d145e577638d08b4a380
Ander Liu <anderliu0216@gmail.com>
2024-05-23 04:17:10 -0500

    Create Sqrt(x).cpp

---
+++ code/Sqrt(x)/Sqrt(x).cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    const int MAX_VALUE = 46340;
+
+public:
+    int mySqrt(int x) {
+        int left = 0, right = MAX_VALUE;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (mid * mid == x) {
+                return mid;
+            } else if (mid * mid < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // right*right < x < left*left
+
+        return right;
+    }
+};
