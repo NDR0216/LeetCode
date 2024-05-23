7e0481c66547dbeabf4e40d63a3452302f3bd9bf
Ander Liu <anderliu0216@gmail.com>
2024-05-22 20:41:40 -0500

    Update NumberOfProvinces.cpp

--- code/NumberOfProvinces/NumberOfProvinces.cpp
+++ code/NumberOfProvinces/NumberOfProvinces.cpp
@@ -1,62 +1,28 @@
-class UnionFind {
-private:
-    int *root, *rank;
-
+class Solution {
 public:
-    int num_root;
-
-    UnionFind(int n) {
-        root = new int[n]; // root array
-        rank = new int[n]();
-        num_root = n;
-
+    void DFS(int v, int n, bool* visited, const vector<vector<int>>& isConnected) {
+        visited[v] = true;
+        
         for (int i = 0; i < n; i++) {
-            root[i] = i;
-        }
-    }
-
-    int find(int x) {
-        if (x != root[x]) { // x is not the root node
-            root[x] = find(root[x]);
-        }
-
-        return root[x];
-    }
-
-    void unionSet(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX != rootY) {
-            num_root--;
-
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
+            if (isConnected[v][i] == 1 && !visited[i]) {
+                DFS(i, n, visited, isConnected);
             }
         }
     }
-};
 
-class Solution {
-public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
 
-        UnionFind uf(n);
+        bool* visited = new bool[n]();
 
+        int num = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (isConnected[i][j]) {
-                    uf.unionSet(i, j);
-                }
+            if (!visited[i]) {
+                DFS(i, n, visited, isConnected);
+                num++;
             }
         }
 
-        return uf.num_root;
+        return num;
     }
 };
