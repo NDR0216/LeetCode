1c3f34d47df62032db33fde22f8fa9422be2f11f
Ander Liu <anderliu0216@gmail.com>
2024-06-02 02:01:43 -0500

    Update SortItemsByGroupsRespectingDependencies.cpp

--- code/SortItemsByGroupsRespectingDependencies/SortItemsByGroupsRespectingDependencies.cpp
+++ code/SortItemsByGroupsRespectingDependencies/SortItemsByGroupsRespectingDependencies.cpp
@@ -2,12 +2,27 @@ class Solution {
 public:
     vector<int> sortItems(int n, int m, vector<int>& group,
                           vector<vector<int>>& beforeItems) {
-        vector<int>* graph = new vector<int>[n];
-        int* indegree = new int[n]();
+        // assign new group
+        for (int i = 0; i < group.size(); i++) {
+            if (group[i] == -1) {
+                group[i] = m;
+                m++;
+            }
+        }
+
+        vector<int>* group_idx = new vector<int>[m];
+        for (int i = 0; i < n; i++) {
+            group_idx[group[i]].push_back(i);
+        }
+
+        // Kahn
+        vector<int>* graph_item = new vector<int>[n];
+        vector<int>* graph_group = new vector<int>[m];
+        int* indegree_item = new int[n]();
         int* indegree_group = new int[m]();
 
         for (int i = 0; i < beforeItems.size(); i++) {
-            indegree[i] = beforeItems[i].size();
+            indegree_item[i] = beforeItems[i].size();
 
             for (int j = 0; j < beforeItems[i].size(); j++) {
                 int a_i = beforeItems[i][j];
@@ -15,97 +30,62 @@ public:
                 int group_a = group[a_i];
                 int group_b = group[b_i];
 
-                graph[a_i].push_back(b_i);
+                graph_item[a_i].push_back(b_i);
 
-                if (group_b != -1 && group_a != group_b) {
+                if (group_a != group_b) {
+                    graph_group[group_a].push_back(group_b);
                     indegree_group[group_b] += 1;
                 }
             }
         }
 
-        vector<vector<int>> group_(m, vector<int>());
-        for (int i = 0; i < n; i++) {
-            if (group[i] != -1) {
-                group_[group[i]].push_back(i);
-            }
-        }
-
-        queue<pair<bool, int>> indegree_0;
+        queue<int> indegree_0_group;
         for (int i = 0; i < m; i++) {
             if (indegree_group[i] == 0) {
-                indegree_0.push({true, i});
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            int group_id = group[i];
-
-            if (group_id == -1 && indegree[i] == 0) {
-                indegree_0.push({false, i});
+                indegree_0_group.push(i);
             }
         }
 
         vector<int> order;
 
-        while (!indegree_0.empty()) {
-            pair<bool, int> p = indegree_0.front();
-            indegree_0.pop();
+        // topo on group
+        while (!indegree_0_group.empty()) {
+            int group_id = indegree_0_group.front();
+            indegree_0_group.pop();
 
-            if (!p.first) { // group_id == -1
-                int v = p.second;
+            for (int i = 0; i < graph_group[group_id].size(); i++) {
+                int u = graph_group[group_id][i];
+
+                indegree_group[u] -= 1;
+
+                if (indegree_group[u] == 0) {
+                    indegree_0_group.push(u);
+                }
+            }
+
+            // topo on item
+            queue<int> indegree_0_item;
+            for (int i = 0; i < group_idx[group_id].size(); i++) {
+                int idx = group_idx[group_id][i];
+
+                if (indegree_item[idx] == 0) {
+                    indegree_0_item.push(idx);
+                }
+            }
+
+            while (!indegree_0_item.empty()) {
+                int v = indegree_0_item.front();
+                indegree_0_item.pop();
 
                 order.push_back(v);
 
-                for (int i = 0; i < graph[v].size(); i++) {
-                    int u = graph[v][i];
-                    int group_id = group[u];
+                for (int i = 0; i < graph_item[v].size(); i++) {
+                    int u = graph_item[v][i];
 
-                    indegree[u] -= 1;
-                    if (group_id != -1) {
-                        indegree_group[group_id] -= 1;
-                    }
+                    indegree_item[u] -= 1;
 
-                    if (group_id == -1 && indegree[u] == 0) {
-                        indegree_0.push({false, u});
-                    } else if (group_id != -1 &&
-                               indegree_group[group_id] == 0) {
-                        indegree_0.push({true, group_id});
-                    }
-                }
-            } else {
-                int group_id = p.second;
-
-                queue<int> curr_group;
-
-                for (int i = 0; i < group_[group_id].size(); i++) {
-                    int idx = group_[group_id][i];
-
-                    if (indegree[idx] == 0) {
-                        curr_group.push(idx);
-                    }
-                }
-
-                while (!curr_group.empty()) {
-                    int vv = curr_group.front();
-                    curr_group.pop();
-
-                    order.push_back(vv);
-
-                    for (int i = 0; i < graph[vv].size(); i++) {
-                        int u = graph[vv][i];
-
-                        indegree[u] -= 1;
-                        if (group[u] != -1 && group[u] != group_id) {
-                            indegree_group[group[u]] -= 1;
-                        }
-
-                        if (indegree[u] == 0 && group[u] == group_id) {
-                            curr_group.push(u);
-                        } else if (group[u] == -1 && indegree[u] == 0) {
-                            indegree_0.push({false, u});
-                        } else if (group[u] != -1 && group[u] != group_id &&
-                                   indegree_group[group[u]] == 0) {
-                            indegree_0.push({true, group[u]});
-                        }
+                    if (indegree_item[u] == 0 && group[u] == group_id) {
+                        indegree_0_item.push(u);
                     }
                 }
             }
