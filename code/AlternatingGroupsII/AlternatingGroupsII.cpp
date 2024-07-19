11757eae42f481ec35993a0615d718e2757a2bf8
Ander Liu <anderliu0216@gmail.com>
2024-07-19 00:43:46 -0500

    Create AlternatingGroupsII.cpp

---
+++ code/AlternatingGroupsII/AlternatingGroupsII.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n = colors.size();
+
+        int group = 0;
+
+        int cnt = 1;
+        // till [n-1, n+k-1)
+        for (int i = 1; i < n + k - 1; i++) {
+            if (colors[(i - 1) % n] != colors[i % n]) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+
+            if (cnt >= k) {
+                group++;
+            }
+        }
+
+        return group;
+    }
+};
