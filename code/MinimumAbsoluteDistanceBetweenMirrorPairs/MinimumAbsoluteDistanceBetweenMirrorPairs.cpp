2255c03c19490e476c02d2ee73c95208443e4c30
Ander Liu <anderliu0216@gmail.com>
2026-04-24 16:39:35 +0800

    Implement mirrorDistance and reverse functions

---
+++ code/MinimumAbsoluteDistanceBetweenMirrorPairs/MinimumAbsoluteDistanceBetweenMirrorPairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int r = 0;
+        while (x > 0) {
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
+
+        return r;
+    }
+
+    int mirrorDistance(int n) {
+        return abs(n - reverse(n));
+    }
+};
