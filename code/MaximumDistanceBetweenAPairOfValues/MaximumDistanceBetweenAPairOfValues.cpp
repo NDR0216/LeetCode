d7fdcbeb2ad945aa2ec5496af41c4b1d430ded4b
Ander Liu <anderliu0216@gmail.com>
2026-04-25 02:04:00 +0800

    Fix formatting and spacing in MaximumDistanceBetweenAPairOfValues.cpp

--- code/MaximumDistanceBetweenAPairOfValues/MaximumDistanceBetweenAPairOfValues.cpp
+++ code/MaximumDistanceBetweenAPairOfValues/MaximumDistanceBetweenAPairOfValues.cpp
@@ -7,12 +7,12 @@ public:
 
         int j = 0;
         for (int i = 0; i < nums1.size(); i++) {
-            while (j+1 < nums2.size() && nums1[i] <= nums2[j+1]) {
+            while (j + 1 < nums2.size() && nums1[i] <= nums2[j + 1]) {
                 j++;
             }
 
             if (i <= j && nums1[i] <= nums2[j]) {
-                max_dist= max(max_dist, j-i);
+                max_dist = max(max_dist, j - i);
             }
         }
 
