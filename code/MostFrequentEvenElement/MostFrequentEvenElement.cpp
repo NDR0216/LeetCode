ec9a2e73ecf3c9a46c3d0ef309313aaa8e923942
Ander Liu <anderliu0216@gmail.com>
2026-04-28 20:14:45 +0800

    Fix frequency counting for most frequent even element

--- code/MostFrequentEvenElement/MostFrequentEvenElement.cpp
+++ code/MostFrequentEvenElement/MostFrequentEvenElement.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
         map<int, int> freq;
-        
+
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] % 2 == 0) {
-                freq[nums[i]] += 1; 
+                freq[nums[i]] += 1;
             }
         }
-        
+
         int max = 0;
         int element = -1;
-        
-        for (map<int, int>::iterator iter = freq.begin(); iter != freq.end(); ++iter) {
-            if (iter->second > max) {
-                max = iter->second;
-                element = iter->first;
+
+        for (map<int, int>::iterator it = freq.begin(); it != freq.end();
+             ++it) {
+            if (it->second > max) {
+                max = it->second;
+                element = it->first;
             }
         }
-        
+
         return element;
     }
 };
