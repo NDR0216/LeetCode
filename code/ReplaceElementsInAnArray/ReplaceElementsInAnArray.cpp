1d112e80cadf3b30ae5ef1e0ba47962675ba3abf
Ander Liu <anderliu0216@gmail.com>
2026-04-28 19:51:36 +0800

    Refactor arrayChange function for clarity and efficiency

--- code/ReplaceElementsInAnArray/ReplaceElementsInAnArray.cpp
+++ code/ReplaceElementsInAnArray/ReplaceElementsInAnArray.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+    vector<int> arrayChange(vector<int>& nums,
+                            vector<vector<int>>& operations) {
         map<int, int> dict;
-        
-        for (int i = 0; i < operations.size(); i++) {     
-            if (dict.find(operations[i][0]) != dict.end()) {
+
+        for (int i = 0; i < operations.size(); i++) {
+            map<int, int>::iterator it = dict.find(operations[i][0]);
+
+            if (it != dict.end()) {
                 dict[operations[i][1]] = dict[operations[i][0]];
-                dict.erase(operations[i][0]); 
+                dict.erase(operations[i][0]);
             } else {
                 dict[operations[i][1]] = operations[i][0];
             }
-            
         }
-        
+
         map<int, int> dict_reversed;
-        
-        for (map<int,int>::iterator i = dict.begin(); i != dict.end(); ++i) {
-            dict_reversed[i->second] = i->first;
+
+        for (map<int, int>::iterator it = dict.begin(); it != dict.end();
+             ++it) {
+            dict_reversed[it->second] = it->first;
         }
-        
+
         for (int i = 0; i < nums.size(); i++) {
-            if (dict_reversed.find(nums[i]) != dict_reversed.end()) {
+            map<int, int>::iterator it = dict_reversed.find(nums[i]);
+
+            if (it != dict_reversed.end()) {
                 nums[i] = dict_reversed[nums[i]];
             }
         }
-        
+
         return nums;
     }
 };
