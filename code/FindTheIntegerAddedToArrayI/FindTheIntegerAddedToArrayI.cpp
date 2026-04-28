3e536af495709f2ed23fe5329ab674c162a55b74
Ander Liu <anderliu0216@gmail.com>
2026-04-29 03:26:34 +0800

    Revert "Refactor min calculation for addedInteger function"
    This reverts commit 6a32e54316a4fccff65dba55861d4322ba130bb6.

--- code/FindTheIntegerAddedToArrayI/FindTheIntegerAddedToArrayI.cpp
+++ code/FindTheIntegerAddedToArrayI/FindTheIntegerAddedToArrayI.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
-        int min1 = nums1[0];
-        for (int i = 1; i < nums1.size(); i++) {
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+
+        for (int i = 0; i < nums1.size(); i++) {
             min1 = min(min1, nums1[i]);
         }
-
-        int min2 = nums2[0];
-        for (int i = 1; i < nums2.size(); i++) {
+        for (int i = 0; i < nums2.size(); i++) {
             min2 = min(min2, nums2[i]);
         }
 
